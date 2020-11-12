0cfe85ba4422f600a9bd099951c9bcc37fed5cd9
San Nge <ngesa@plu.edu>
2020-11-12 15:51:04 -0800

    fixed menu phone error

--- src/component/Navbar/NavItems/Toggler/Toggler.js
+++ src/component/Navbar/NavItems/Toggler/Toggler.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import classes from './Toggler.module.css'
 function Toggler(props) {
-    let CLASS=[props.click?'fas fa-times' : 'fas','fa-bars',classes.Toggler];
+    let CLASS=[props.click ? 'fas fa-times' : 'fas',props.click? classes.active:'','fa-bars',classes.Toggler];
+    // let CLASS = props.click ? ['fas','fa-times',classes.Toggler,classes.active] : ['fas','fa-bars',classes.Toggler]
     let color='242424';
     if(props.click){
         color='#f4f4f4';
