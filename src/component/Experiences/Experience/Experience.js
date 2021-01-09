4fd6f93941d58e8124458838a98bc53ccdf05cab
San Nge <samngestep@gmail.com>
2021-01-08 23:30:53 -0800

    updated project infos

--- src/component/Experiences/Experience/Experience.js
+++ src/component/Experiences/Experience/Experience.js
@@ -1,95 +1,92 @@
-import React,{useState,useEffect} from 'react'
-import classes from './Experience.module.css'
-import moment from 'moment'
+import React, { useState, useEffect } from "react";
+import classes from "./Experience.module.css";
+import moment from "moment";
 
-function Experience({exp: {at,title,from,to,location,desc,image}}) {
-    const [duration,setDuration] = useState("");
-    const monthDiff = (d1,d2) => {
-        let t = null;
-        if(to == 'Present') {
-            t = new Date();
-        }else {
-            t = to;
-        }
-        let months = 0;
-        let year = 0;
-        let total = "";
-       if(from.getFullYear() !== t.getFullYear()) {
-           year = Math.abs(t.getFullYear() - from.getFullYear())
-       }
-       if(from.getMonth() - t.getMonth() === 0) {
-           months = 1
-       }else {
-        //    if(t.getMonth() - from.getMonth() <0) {
-        //     months = Math.abs(t.getMonth() - from.getMonth())
-        //     year = year - 1;
-        //    }else {
-        //     months = Math.abs(t.getMonth() - from.getMonth())
-        //    }
-        if(from.getMonth() - t.getMonth() > 0 ) {
-            months = 12 - from.getMonth() + to.getMonth()
-            year = year - 1;
-        }
-        else {
-            months = months = t.getMonth() - from.getMonth()
-        }
-           
-       }
+function Experience({ exp: { at, title, from, to, location, desc, image } }) {
+	const [duration, setDuration] = useState("");
+	const monthDiff = (d1, d2) => {
+		let t = null;
+		if (to == "Present") {
+			t = new Date();
+		} else {
+			t = to;
+		}
+		let months = 0;
+		let year = 0;
+		let total = "";
+		if (from.getFullYear() !== t.getFullYear()) {
+			year = Math.abs(t.getFullYear() - from.getFullYear());
+		}
+		if (from.getMonth() - t.getMonth() === 0) {
+			months = 1;
+		} else {
+			//    if(t.getMonth() - from.getMonth() <0) {
+			//     months = Math.abs(t.getMonth() - from.getMonth())
+			//     year = year - 1;
+			//    }else {
+			//     months = Math.abs(t.getMonth() - from.getMonth())
+			//    }
+			if (from.getMonth() - t.getMonth() > 0) {
+				months = 12 - from.getMonth() + t.getMonth();
+				year = year - 1;
+			} else {
+				months = months = t.getMonth() - from.getMonth();
+			}
+		}
 
-       if(year == 0) {
-           if(months > 1) {
-            total = `${months+1} months`
-           }else {
-            total = `${months} month`
-           }
-           
-       }
-       else {
-           if(year > 1) {
-               if(months > 1) {
-                total = `${year} years, ${months} months`
-               }else {
-                total = `${year} years, ${months} month`
-               }
-           }else {
-               if(months > 1) {
-                total = `${year} year, ${months} months`
-               }else {
-                total = `${year} year, ${months} month`
-               }
-           }
-           
-       }
-       setDuration(total);
+		if (year == 0) {
+			if (months > 1) {
+				total = `${months + 1} months`;
+			} else {
+				total = `${months} month`;
+			}
+		} else {
+			if (year > 1) {
+				if (months > 1) {
+					total = `${year} years, ${months} months`;
+				} else {
+					total = `${year} years, ${months} month`;
+				}
+			} else {
+				if (months > 1) {
+					total = `${year} year, ${months} months`;
+				} else {
+					total = `${year} year, ${months} month`;
+				}
+			}
+		}
+		setDuration(total);
+	};
+	useEffect(() => {
+		monthDiff(from, to);
+	}, []);
 
-
-    }
-    useEffect(() => {
-        monthDiff(from,to)
-        
-    }, []);
-
-    return (
-        <div className={classes.Experience}>
-        <div className={classes.innerContainer}>
-         <div className={classes.imageDiv}>
-           <img src={image} alt=""/>
-        </div>
-        <div className={classes.bodyDiv}>
-          <h3>{at}</h3>
-          <h4>{title}</h4>
-          <small>{moment(from).format('MMMM YYYY')} - {to === 'Present' ? <>Present</> : moment(to).format('MMMM YYYY')} <span className={classes.duration}>({duration})</span></small>
-          <div className={classes.location}><small>{location}</small></div>
-          <ul>
-              {desc.map((d,index) => (
-                  <li key={index}>{d}</li>
-              ))}
-          </ul>
-
-        </div>
-        </div>
-        </div>
-    )
+	return (
+		<div className={classes.Experience}>
+			<div className={classes.innerContainer}>
+				<div className={classes.imageDiv}>
+					<img src={image} alt='' />
+				</div>
+				<div className={classes.bodyDiv}>
+					<h3>{at}</h3>
+					<h4>{title}</h4>
+					<small>
+						{moment(from).format("MMMM YYYY")} -{" "}
+						{to === "Present" ? <>Present</> : moment(to).format("MMMM YYYY")}{" "}
+						<span className={classes.duration}>({duration})</span>
+					</small>
+					<div className={classes.location}>
+						<small>{location}</small>
+					</div>
+					<ul>
+						{desc.map((d, index) => (
+							<li key={index}>{d}</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Experience
+export default Experience;
--- src/container/pages/Projects/ProjectPage.js
+++ src/container/pages/Projects/ProjectPage.js
@@ -1,74 +1,132 @@
-import React,{useState} from 'react'
-import classes from './ProjectPage.module.css'
-import Projects from '../../../component/Projects/Projects'
-import SearchBar from '../../../component/SearchBar/SearchBar'
-import BurgerProject from '../../../assets/project-images/burgerBuilderProject.png'
-import Connect6Project from '../../../assets/project-images/connect6Project.png'
-import xResearchProject from '../../../assets/project-images/xResearchProject.png'
-import Refmonkey from '../../../assets/project-images/Refmonkey.png'
+import React, { useState } from "react";
+import classes from "./ProjectPage.module.css";
+import Projects from "../../../component/Projects/Projects";
+import SearchBar from "../../../component/SearchBar/SearchBar";
+import BurgerProject from "../../../assets/project-images/burgerBuilderProject.png";
+import Connect6Project from "../../../assets/project-images/connect6Project.png";
+import xResearchProject from "../../../assets/project-images/xResearchProject.png";
+import MERNForm from "../../../assets/project-images/MERNMultiStepForm.png";
+import Socix from "../../../assets/project-images/SocixProject.png";
+
+import Refmonkey from "../../../assets/project-images/Refmonkey.png";
 function ProjectPage() {
-    const [text,setText] = useState("")
-    const [submitted,setSubmitted] = useState(false)
-    const [paginateData,setPaginateData] = useState({
-        offset: 0,
-        tableData: [],
-        orgTableData: [],
-        perPage:1,
-        currentPage:0
-    })
-    const [state] = useState([
-        {
-            image: BurgerProject,
-            header: "Burger Builder Web Application (React & Firebase)",
-            tags: ['ReactJS', 'Redux', 'CSS3', 'REST','HTML5', 'Realtime', 'Firebase'],
-            desc: [
-                'A Single Page Fully Functional Web Application for Burger Ordering',
-                'Users can visualize freshly-built  burger in real-time',
-                'Includes secure Authentication'
-            ],
-            source: 'https://github.com/sannge/Burger-builder-Ractjs-'
-        },
-        {
-            image: Connect6Project,
-            header: "Connect 6 Multiplayer Game",
-            tags: ['Javascript', 'Java', 'REST', 'HTML5', 'CSS3'],
-            desc: [
-                'A Multi-Page Fully Functional Connect-6 Game',
-                'Allows live view of the current games to other players',
-                'Includes secure Authentication and Leaderboard System',
-            ],
-            source: 'https://github.com/sannge/CunningCoders'
-        },
-        {
-            image: xResearchProject,
-            header: "X-Research Desktop Application (Cross Platform)",
-            tags: ['AI','ElectronJS', 'Javascript', 'Java', 'REST', 'HTML5', 'CSS3'],
-            desc: [
-                'AI based Research Paper Generator',
-                'Automatic Citation and Bibliography generation on Paper Creation',
-                'Built-in text-summarizer',
-            ],
-            source: 'https://github.com/sannge/CunningCoders'
-        },
-        {
-            image: Refmonkey,
-            header: "RefMonkey SAAS Web Application (Co-Developer)",
-            tags: ['ReactJS', 'GraphQL', 'HTML5', 'CSS3', 'MongoDB', 'ExpressJS'],
-            desc: [
-                'Referral Web Application/Business for Online Stores and Businesses',
-                'Built features; edit profile settings, multiple langauge feature, Ad-Attribution System, and more ... '
-            ],
-            source: 'https://refmonkey.com'
-        },
+	const [text, setText] = useState("");
+	const [submitted, setSubmitted] = useState(false);
+	const [paginateData, setPaginateData] = useState({
+		offset: 0,
+		tableData: [],
+		orgTableData: [],
+		perPage: 1,
+		currentPage: 0,
+	});
+	const [state] = useState([
+		{
+			image: Socix,
+			header: "Social Media  Web Application (On Progress)",
+			tags: [
+				"ReactJS",
+				"ContextAPI",
+				"MySQL",
+				"Sequelize",
+				"GraphQL",
+				"GraphQLSubscription",
+				"WebRTC",
+				"Express",
+				"Node.js",
+				"MaterialUI",
+			],
+			desc: [
+				"A Single Page MERNG stack Social Media Application with real time data transfer",
+				"Implemented using latest technologies such as GraphQL Subscriptions, WebRTC to be able to receive and update live information",
+				"Secure Authentication",
+			],
+			source: "https://github.com/sannge/Socix-for-Social",
+		},
+		{
+			image: MERNForm,
+			header: "Athelete Profile Creator with MultiStep Form",
+			tags: [
+				"Javascript",
+				"React",
+				"Express",
+				"MongoDB",
+				"Node.js",
+				"Heroku",
+				"GLocationService",
+			],
+			desc: [
+				"MERN stack Athelete Profile Bank",
+				"Multistep Form Automation",
+				"Google Map Location API to autocomplete, and throw error on non-existing Locations",
+			],
+			source: "https://mighty-sands-65048.herokuapp.com",
+		},
+		{
+			image: Refmonkey,
+			header: "RefMonkey SAAS Web Application (Co-Developer)",
+			tags: ["ReactJS", "GraphQL", "HTML5", "CSS3", "MongoDB", "ExpressJS"],
+			desc: [
+				"Referral Web Application/Business for Online Stores and Businesses",
+				"Built features; edit profile settings, multiple langauge feature, Ad-Attribution System, and more ... ",
+			],
+			source: "https://refmonkey.com",
+		},
+		{
+			image: BurgerProject,
+			header: "Burger Builder Web Application (React & Firebase)",
+			tags: [
+				"ReactJS",
+				"Redux",
+				"CSS3",
+				"REST",
+				"HTML5",
+				"Realtime",
+				"Firebase",
+			],
+			desc: [
+				"A Single Page Fully Functional Web Application for Burger Ordering",
+				"Users can visualize freshly-built  burger in real-time",
+				"Includes secure Authentication",
+			],
+			source: "https://github.com/sannge/Burger-builder-Ractjs-",
+		},
+		{
+			image: Connect6Project,
+			header: "Connect 6 Multiplayer Game",
+			tags: ["Javascript", "Java", "REST", "HTML5", "CSS3"],
+			desc: [
+				"A Multi-Page Fully Functional Connect-6 Game",
+				"Allows live view of the current games to other players",
+				"Includes secure Authentication and Leaderboard System",
+			],
+			source: "https://github.com/sannge/CunningCoders",
+		},
+		{
+			image: xResearchProject,
+			header: "X-Research Desktop Application (Cross Platform)",
+			tags: ["AI", "ElectronJS", "Javascript", "Java", "REST", "HTML5", "CSS3"],
+			desc: [
+				"AI based Research Paper Generator",
+				"Automatic Citation and Bibliography generation on Paper Creation",
+				"Built-in text-summarizer",
+			],
+			source: "https://github.com/sannge/CunningCoders",
+		},
+	]);
 
-    ])
-
-    return (
-
-        <div className={classes.Projects}>
-           <Projects paginateData={paginateData} setPaginateData={setPaginateData} submitted={submitted} setSubmitted={setSubmitted} state={state} text={text} setText={setText}/>
-        </div>
-    )
+	return (
+		<div className={classes.Projects}>
+			<Projects
+				paginateData={paginateData}
+				setPaginateData={setPaginateData}
+				submitted={submitted}
+				setSubmitted={setSubmitted}
+				state={state}
+				text={text}
+				setText={setText}
+			/>
+		</div>
+	);
 }
 
-export default ProjectPage
+export default ProjectPage;
