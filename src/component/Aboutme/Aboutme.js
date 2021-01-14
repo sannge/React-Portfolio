5bc23aec09b6b874081c4c8fe767c657bb98fb00
San Nge <samngestep@gmail.com>
2021-01-14 00:22:35 -0800

    updated resume

--- src/component/Aboutme/Aboutme.js
+++ src/component/Aboutme/Aboutme.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import classes from './Aboutme.module.css'
+import React from "react";
+import classes from "./Aboutme.module.css";
 function Aboutme() {
-    return (
-        <div className={classes.AboutmeContainer}>
-            <h2>About</h2>
-            <div className={classes.AboutmeMainAndProfile}>
-                {/* <div className={classes.AboutmeImage}>
+	return (
+		<div className={classes.AboutmeContainer}>
+			<h2>About</h2>
+			<div className={classes.AboutmeMainAndProfile}>
+				{/* <div className={classes.AboutmeImage}>
                     <img src={AboutmeProfile} alt="profile"/>
                 </div> */}
-            <div className={classes.AboutmeMain}>
-            <p>I am a proactive student (BS in Computer Science)
-                currently attending Pacific Lutheran University graduating 
-                in December 2020. I spend most of my time learning new topics 
-                on web development and building projects with them. I am comfortable 
-                building MERN stack web applications and familiar building chat applications,
-                e-commerce websites, and more. My hobbies are Coding, Hiking, and Cooking.
-            </p>
-            </div>
-            </div>
-            
-        </div>
-        
-    )
+				<div className={classes.AboutmeMain}>
+					<p>
+						I am a proactive student (BS in Computer Science) currently
+						attending Pacific Lutheran University graduating in December 2020. I
+						spend most of my time learning new topics on web development and
+						building projects with them. I am comfortable building MERN stack
+						web applications and familiar building chat applications, e-commerce
+						websites, and more. My hobbies are Coding, Hiking, and Cooking.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Aboutme
+export default Aboutme;
