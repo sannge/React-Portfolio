b7abc537f25ba690c1bbb6cde5e762dca057dd21
San Nge <samngestep@gmail.com>
2021-02-09 23:36:34 -0800

    changed footer

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,20 +1,40 @@
-import React from 'react'
-import classes from './Footer.module.css'
-import {Link} from 'react-router-dom'
+import React from "react";
+import classes from "./Footer.module.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
-    return (
-        <div className={classes.Footer}>
-            <Link className={classes.Link} to='/'><h1>San</h1></Link>
-            <div className={classes.social}>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/sam.nge.507"> <i className='fab fa-facebook'></i></a>
-               
-            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/san-nge-695b16180"> <i className='fab fa-linkedin'></i></a>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.github.com/sannge"> <i className='fab fa-github'></i></a>
-            </div>
-            <small>&copy; 2020 Copyright</small>
-        </div>
-    )
+	return (
+		<div className={classes.Footer}>
+			<Link className={classes.Link} to='/'>
+				<h1>San</h1>
+			</Link>
+			<div className={classes.social}>
+				<a
+					target='_blank'
+					rel='noopener noreferrer'
+					href='https://www.facebook.com/sam.nge.507'>
+					{" "}
+					<i className='fab fa-facebook'></i>
+				</a>
+
+				<a
+					target='_blank'
+					rel='noopener noreferrer'
+					href='https://www.linkedin.com/in/san-nge-695b16180'>
+					{" "}
+					<i className='fab fa-linkedin'></i>
+				</a>
+				<a
+					target='_blank'
+					rel='noopener noreferrer'
+					href='https://www.github.com/sannge'>
+					{" "}
+					<i className='fab fa-github'></i>
+				</a>
+			</div>
+			<small>&copy; 2021 Copyright</small>
+		</div>
+	);
 }
 
-export default Footer
+export default Footer;
